32408c49ff3b126d99aa36f7111fa4c4489a43f5
loringabriel <loringabriel@gmail.com>
2022-04-27 16:09:18 +0300

    add csr

---
+++ pages/csr.js
@@ -0,0 +1,15 @@
+export default function CsrPage() {
+  return (
+    <>
+      <div>CSR TESTs</div>
+      <style jsx>
+        {`
+          div {
+            background: lightblue;
+            padding: 30px;
+          }
+        `}
+      </style>
+    </>
+  );
+}
